c10ac4ae4fc47e85b7a95fd9d739e10f56d9a169
Zac <zac_hill@utulsa.edu>
2012-06-12 13:07:44 -0500

    Constants reversion and fix

--- src/Constants.java
+++ src/Constants.java
@@ -9,7 +9,9 @@ public class Constants {
 	public static int _numnodes = 500;
 	public static int _avgdegree = 5;
 	public static int _edges = (_numnodes * _avgdegree)/2;
-	public static int _groups = _numnodes / 50;
+	public static int groupRatio = 50;
+	public static int _groups = _numnodes / groupRatio;
+	public static boolean DynamicGroups = false;
 	
 	public double _SIM_epsilon_start = .025;
 	public double _SIM_epsilon_final = 1;
@@ -19,13 +21,13 @@ public class Constants {
 	public static int _trials = 50;
 	public double _epsilon = _SIM_epsilon_start;
 	public static boolean _murand = false;
-	public static final double _mu = .5;
+	public static double _mu = .5;
 	
-	public static double _p_ext = (double)_avgdegree / (_numnodes-1);
+	public static double _p_ext = (double)_avgdegree / (_numnodes-1) * 2;
 	public static boolean Repulsive = false;
 	public static int repuslivePer = 50;
 	public static boolean ConstantEp = false;
-	public static int numThreads = 4;
+	public static int numThreads = 3;
 	public static boolean verbose = false; //Specifies if any threads are verbose
 	public static int threadVerbose = -1; // Specifies a specific thread num to be verbose(only one)
 	public static int numThreadsVerbose = 1; // Specifies a number of threads to be verbose
@@ -34,8 +36,9 @@ public class Constants {
 	
 	//stuff for data collection
 	public static String _OUTPUT_PATH = "test\\";
-	public final double threshold = _epsilon * _mu / 10;
-	public final static boolean debug = true;
+	public final double threshold = _epsilon * _mu / 100;
+	public static int minAgents = 5;
+	public final static boolean debug = false;
 	public static ArrayList<String> files = new ArrayList<String>();
 	public static double randMuStart = 0;
 	public static double randMuEnd = 1;
